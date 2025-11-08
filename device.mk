6034de744378b05855b00c67654d801ddf00717f
mrfluffyoven <mrfluffyoven@gmail.com>
2025-11-08 08:02:43 +0000

    add the ability to flash zips
    Change-Id: I67e3596c83521b9e414b69035346d3fa9f13f20b

--- device.mk
+++ device.mk
@@ -36,6 +36,12 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
